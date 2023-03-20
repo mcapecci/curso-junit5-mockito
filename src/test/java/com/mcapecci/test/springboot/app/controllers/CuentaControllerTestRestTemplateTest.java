01d291cfdcae499bd4448a7c809213730ecdb3dd
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-20 16:51:14 -0300

    Pruebas de integración con RestTemplate

---
+++ src/test/java/com/mcapecci/test/springboot/app/controllers/CuentaControllerTestRestTemplateTest.java
@@ -0,0 +1,84 @@
+package com.mcapecci.test.springboot.app.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mcapecci.test.springboot.app.models.Cuenta;
+import com.mcapecci.test.springboot.app.models.TransaccionDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CuentaControllerTestRestTemplateTest {
+
+    @Autowired
+    private TestRestTemplate client;
+
+    private ObjectMapper objectMapper;
+
+    @LocalServerPort
+    private int puerto;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @Order(1)
+    void testTransferir() throws JsonProcessingException {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setMonto(new BigDecimal("100"));
+        dto.setCuentaDestinoId(2L);
+        dto.setCuentaOrigenId(1L);
+        dto.setBancoId(1L);
+
+        ResponseEntity<String> response = client.
+                postForEntity(crearUri("/api/cuentas/transferir"), dto, String.class);
+        System.out.println(puerto);
+        String json = response.getBody();
+        System.out.println(json);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        assertNotNull(json);
+        assertTrue(json.contains("Transferencia realizada con éxito!"));
+        assertTrue(json.contains("{\"cuentaOrigenId\":1,\"cuentaDestinoId\":2,\"monto\":100,\"bancoId\":1}"));
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        assertEquals("Transferencia realizada con éxito!", jsonNode.path("mensaje").asText());
+        assertEquals(LocalDate.now().toString(), jsonNode.path("date").asText());
+        assertEquals("100", jsonNode.path("transaccion").path("monto").asText());
+        assertEquals(1L, jsonNode.path("transaccion").path("cuentaOrigenId").asLong());
+
+        Map<String, Object> response2 = new HashMap<>();
+        response2.put("date", LocalDate.now().toString());
+        response2.put("status", "OK");
+        response2.put("mensaje", "Transferencia realizada con éxito!");
+        response2.put("transaccion", dto);
+
+        assertEquals(objectMapper.writeValueAsString(response2), json);
+
+    }
+
+
+    private String crearUri(String uri) {
+        return "http://localhost:" + puerto + uri;
+    }
+}
