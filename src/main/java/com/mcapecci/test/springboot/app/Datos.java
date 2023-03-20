83e0f823ffd83e340bea03135152c8d1d054629f
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-20 13:16:12 -0300

    Escribiendo tests con JUnit y mockito

---
+++ src/main/java/com/mcapecci/test/springboot/app/Datos.java
@@ -0,0 +1,13 @@
+package com.mcapecci.test.springboot.app;
+
+import com.mcapecci.test.springboot.app.models.Banco;
+import com.mcapecci.test.springboot.app.models.Cuenta;
+
+import java.math.BigDecimal;
+
+public class Datos {
+    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Andrés", new BigDecimal("1000"));
+    public static final Cuenta CUENTA_002 = new Cuenta(2L, "Jhon", new BigDecimal("2000"));
+    public static final Banco BANCO = new Banco(1L, "El banco financiero", 0);
+
+}
--- src/test/java/com/mcapecci/test/springboot/app/SpringbootTestApplicationTest.java
+++ src/test/java/com/mcapecci/test/springboot/app/SpringbootTestApplicationTest.java
@@ -1,15 +1,55 @@
 package com.mcapecci.test.springboot.app;
 
+import com.mcapecci.test.springboot.app.repositories.BancoRepository;
+import com.mcapecci.test.springboot.app.repositories.CuentaRepository;
+import com.mcapecci.test.springboot.app.services.CuentaService;
+import com.mcapecci.test.springboot.app.services.CuentaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ServiceApplicationTest {
+    CuentaRepository cuentaRepository;
+
+
+    BancoRepository bancoRepository;
+
+
+    CuentaService service;
+
+    @BeforeEach
+    void setUp() {
+        cuentaRepository = mock(CuentaRepository.class);
+        bancoRepository = mock(BancoRepository.class);
+        service = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+    }
 
     @Test
     void contextLoads() {
+        when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+        when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+        when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+
+        BigDecimal saldoOrigen = service.revisarSaldo(1L);
+        BigDecimal saldoDestino = service.revisarSaldo(2L);
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
+
+        service.transferir(1L, 2L, new BigDecimal("100"), 1L);
+
+        saldoOrigen = service.revisarSaldo(1L);
+        saldoDestino = service.revisarSaldo(2L);
+
+        assertEquals("900", saldoOrigen.toPlainString());
+        assertEquals("2100", saldoDestino.toPlainString());
+
     }
 
 }
