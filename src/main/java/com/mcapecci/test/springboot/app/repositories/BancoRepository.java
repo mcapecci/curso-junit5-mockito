88fb12f61558de4867255a2bc2925a8fea0c4962
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-20 14:07:30 -0300

    Modificando nuestros repositorios con Spring Data JPA

--- src/main/java/com/mcapecci/test/springboot/app/repositories/BancoRepository.java
+++ src/main/java/com/mcapecci/test/springboot/app/repositories/BancoRepository.java
@@ -2,14 +2,13 @@ package com.mcapecci.test.springboot.app.repositories;
 
 
 import com.mcapecci.test.springboot.app.models.Banco;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BancoRepository {
-    List<Banco> findAll();
-
-    Banco findById(Long id);
-
-    void update(Banco banco);
+public interface BancoRepository extends JpaRepository<Banco, Long> {
+//    List<Banco> findAll();
+//    Banco findById(Long id);
+//    void update(Banco banco);
 
 }
