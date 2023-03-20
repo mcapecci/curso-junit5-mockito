5336890eb28c43a8d10d99edb50ce5bd46182484
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-20 15:30:51 -0300

    Pruebas unitarias para el controlador con @WebMvcTest y MockMvc

---
+++ src/test/java/com/mcapecci/test/springboot/app/controllers/CuentaControllerTest.java
@@ -0,0 +1,93 @@
+package com.mcapecci.test.springboot.app.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mcapecci.test.springboot.app.models.TransaccionDto;
+import com.mcapecci.test.springboot.app.services.CuentaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.mcapecci.test.springboot.app.Datos.crearCuenta001;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CuentaController.class)
+class CuentaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CuentaService cuentaService;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testDetalle() throws Exception {
+        // Given
+        when(cuentaService.findById(1L)).thenReturn(crearCuenta001().orElseThrow());
+
+        // When
+        mvc.perform(get("/api/cuentas/1").contentType(MediaType.APPLICATION_JSON))
+                // Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.persona").value("Andrés"))
+                .andExpect(jsonPath("$.saldo").value("1000"));
+
+        verify(cuentaService).findById(1L);
+    }
+
+    @Test
+    void testTransferir() throws Exception, JsonProcessingException {
+
+        // Given
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setMonto(new BigDecimal("100"));
+        dto.setBancoId(1L);
+
+        System.out.println(objectMapper.writeValueAsString(dto));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", "OK");
+        response.put("mensaje", "Transferencia realizada con éxito!");
+        response.put("transaccion", dto);
+
+        System.out.println(objectMapper.writeValueAsString(response));
+
+        // When
+        mvc.perform(post("/api/cuentas/transferir")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+
+                // Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.mensaje").value("Transferencia realizada con éxito!"))
+                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(dto.getCuentaOrigenId()))
+                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+
+    }
+
+}
