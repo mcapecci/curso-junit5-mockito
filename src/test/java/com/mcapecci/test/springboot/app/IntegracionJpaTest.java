324b893df8933019b2652f54281a78f007acd0a6
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-20 14:13:18 -0300

    Pruebas de integración con @DataJpaTest

---
+++ src/test/java/com/mcapecci/test/springboot/app/IntegracionJpaTest.java
@@ -0,0 +1,51 @@
+package com.mcapecci.test.springboot.app;
+
+import com.mcapecci.test.springboot.app.models.Cuenta;
+import com.mcapecci.test.springboot.app.repositories.CuentaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class IntegracionJpaTest {
+    @Autowired
+    CuentaRepository cuentaRepository;
+
+    @Test
+    void testFindById() {
+        Optional<Cuenta> cuenta = cuentaRepository.findById(1L);
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+    }
+
+    @Test
+    void testFindByPersona() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Andrés");
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Rod");
+        // metodo de referencia: cuenta::orElseThrow  es igual a () -> cuenta.orElseThrow()
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = cuentaRepository.findAll();
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+    }
+
+}
